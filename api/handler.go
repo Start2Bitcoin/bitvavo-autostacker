4f199ca3968687ec4dc30874245c9041aecce839
kdebacker <ext.kwinten.de.backer@sofico.be>
2021-02-16 14:54:05 +0100

    add handler

--- api/handler.go
+++ api/handler.go
@@ -1,7 +1,29 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	GetEurBalance()
+	euros, err := GetEurBalance()
+	if err != nil {
+		logrus.WithError(err).Error("Something went wrong fetching Bitvavo account balance")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if euros == 0 {
+		logrus.Info("No euros to buy coins")
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	order, err := BuyBitcoin(euros)
+	if err != nil {
+		logrus.WithError(err).Error("Something went wrong buying bitcoin on bitvavo")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	logrus.WithField("order", order.Amount).Info("Stacked some sats")
+	w.WriteHeader(http.StatusOK)
 }
--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/kiwiidb/go-bitvavo-api v1.2.0
 	github.com/koding/multiconfig v0.0.0-20171124222453-69c27309b2d7
+	github.com/sirupsen/logrus v1.7.1
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
